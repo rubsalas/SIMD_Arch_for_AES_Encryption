55ac9c68963bc6fc68108b24289b6db5f980ba90
Rubén Salas <rubsalas@hotmail.com>
2024-09-19 02:52:13 -0600

    fix(memory): Update in wires and instances in memory top module

--- microarchitecture/hdl_files/memory/memory.sv
+++ microarchitecture/hdl_files/memory/memory.sv
@@ -3,33 +3,30 @@ Full Memory module
 Date: 13/09/24
 Test bench ran: XX/09/24
 */
-module memory # (parameter N = 32) (
+module memory # (parameter N = 32, parameter V = 256) (
 		input  logic clk,
+		/* Instruction inputs */
+		input  logic [N-1:0] pc_address,	// from PC to A in instruction memory
+		/* Data inputs */
+		input  logic [N-1:0] address_data,	// MemAddress (Ao from data_aligner) to A
+		input  logic [31:0] byteena_data,	// Control Signal: Byteena from data_aligner
+		input  logic [V-1:0] write_data,	// MemWriteData (WD from data_aligner) to WD
+		input  logic rden_data,				// Control Signal: Rden from data_aligner 
+		input  logic wren_data,				// Control Signal: Wren from data_aligner 
 
-		input  logic [N-1:0] pc_address,			// from PC to A in instruction memory
-		input  logic [N-1:0] data_address_scalar,	// from ALUResult to A in data memory
-	//	input  logic [N-1:0] data_address_vector,	// NYI: not yet implemented
-
-		input  logic [N-1:0] write_data_scalar,		// WriteData (RD2 from register file) to WD
-	//	input  logic [255:0] write_data_vector,		// NYI
-
-		input  logic MemWrite_scalar,				// Control Signal: MemWrite from Control Unit 
-		input  logic MemWrite_vector,				// Control Signal: NYI
-	
-		output logic [N-1:0] instruction,			// to Instruction after instruction memory
-		output logic [N-1:0] read_data_scalar,		// ReadData from RD in data memory to MemtoRegMux
-	//	output logic [255:0] read_data_vector		// NYI
+		/* Instruction outputs */
+		output logic [N-1:0] instruction,	// to Instruction after instruction memory
+		/* Data ouputs */
+		output logic [V-1:0] read_data,		// MemReadData (to RDi from data memory) from RD
 	);
 
 	// Specific addresses for ips
 	logic [13:0] in_address;
-	logic [13:0] sd_address;	// 16384 addresses of 24 bits
-	logic [14:0] vd_address;	// 32768 addresses of 256 bits
+	logic [13:0] dt_address;	// 16384 addresses of 256 bits
 
 	// Bus length adjust
 	assign in_address = pc_address[13:0];
-	assign sd_address = data_address_scalar[13:0];
-	assign vd_address = data_address_vector[14:0];
+	assign dt_address = address_data[13:0];
 
 	
 	/* Instruction memory */
@@ -37,21 +34,13 @@ module memory # (parameter N = 32) (
 								  	   		 .instruction(instruction));
 
 
-	/* Scalar data mamory */
-	data_memory ram_scalar_memory (.address(sd_address),
-								   .clock(clk),
-								   .data(write_data_scalar),
-								   .wren(MemWrite_scalar),
-								   .q(read_data_scalar));
-
-
-	/* Vector Data Memory */ /*
-	ram_vector ram_scalar_memory (.address(vd_address),
-								  .clock(clk),
-								  .data(write_data_vector),
-								  .rden(1'b1),
-								  .wren(MemWrite_vector),
-								  .q(read_data_vector));
-	*/
+	/* Data memory */
+	data_memory ram_memory (.address(dt_address),
+							.byteena(byteena_data),
+							.clock(clk),
+							.data(write_data),
+							.rden(rden_data),
+							.wren(wren_data),
+							.q(read_data));
 
 endmodule
